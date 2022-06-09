104e15d6a262b21a043a772fc1d6f9c26d15c591
magzhan9292 <magzhan9292@gmail.com>
2022-06-09 11:31:13 +0600

    Реализовать защиту от спама в формах обратной связи

--- catalog/controller/common/popups2.php
+++ catalog/controller/common/popups2.php
@@ -2,15 +2,18 @@
 class ControllerCommonPopups2 extends Controller {
 	public function index() {
 		$this->load->language('common/popups2');
-      
+
         $data['name'] = $this->customer->getFirstName();
         $data['email'] = $this->customer->getEmail();
         $data['telephone'] = '';
         $data['action'] = $this->url->link('common/popups2/add', '', true);
         $data['ur'] = $this->config->get('config_ur');
+
+        $data['captcha'] = $this->load->controller('extension/captcha/' . $this->config->get('config_captcha'), $this->error);
+
 		$this->response->setOutput($this->load->view('common/popups2', $data));
 	}
-  
+
     public function add() {
         $this->load->language('common/popups2');
 
@@ -20,19 +23,19 @@ class ControllerCommonPopups2 extends Controller {
             if ((utf8_strlen($this->request->post['name']) < 3) || (utf8_strlen($this->request->post['name']) > 25)) {
 				$json['error']['name'] = $this->language->get('error_name');
 			}
-			
+
 			if ((utf8_strlen($this->request->post['telephone']) < 3) || (utf8_strlen($this->request->post['telephone']) > 32)) {
 			    $json['error']['telephone'] = $this->language->get('error_telephone');
 		    }
-          
+
             if (!filter_var($this->request->post['email'], FILTER_VALIDATE_EMAIL)) {
 			    $json['error']['email'] = $this->language->get('error_email');
 		    }
-			
+
 			if (!isset($this->request->post['agree'])) {
 				$json['error']['agree'] = $this->language->get('error_agree');
 			}
-          
+
             if (!isset($json['error'])) {
 				$this->load->model('catalog/review');
 
@@ -43,7 +46,7 @@ class ControllerCommonPopups2 extends Controller {
             }
 
         }
-      
+
         $this->response->addHeader('Content-Type: application/json');
 		$this->response->setOutput(json_encode($json));
     }
